671997c7547db5b5a135132809345b9942ff186c
Benazeth Léo <leo.benazeth@ynov.com>
2024-04-09 11:45:57 +0200

    déplacer l'affichage dans le fichier ForumScreen

---
+++ forum/src/main/java/com/oop/ForumFrame.java
@@ -0,0 +1,69 @@
+package com.oop;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ForumFrame extends JFrame {
+    private JTextArea chatArea;
+    private JTextField messageField;
+
+    public ForumFrame() {
+        setTitle("Simple Forum");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 400);
+        setLayout(new BorderLayout());
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem openItem = new JMenuItem("Open");
+        JMenuItem saveAsItem = new JMenuItem("Save As");
+        fileMenu.add(openItem);
+        fileMenu.add(saveAsItem);
+        menuBar.add(fileMenu);
+
+        JPanel messagePanel = new JPanel();
+        JLabel messageLabel = new JLabel("Enter Message: ");
+        messageField = new JTextField(20);
+        JButton sendButton = new JButton("Send");
+        JButton resetButton = new JButton("Reset");
+
+        sendButton.addActionListener(new SendButtonListener());
+        resetButton.addActionListener(new ResetButtonListener());
+
+        messagePanel.add(messageLabel);
+        messagePanel.add(messageField);
+        messagePanel.add(sendButton);
+        messagePanel.add(resetButton);
+
+        chatArea = new JTextArea();
+        chatArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(chatArea);
+
+        add(menuBar, BorderLayout.NORTH);
+        add(scrollPane, BorderLayout.CENTER);
+        add(messagePanel, BorderLayout.SOUTH);
+
+        setVisible(true);
+    }
+
+    private class SendButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String message = messageField.getText();
+            if (!message.isEmpty()) {
+                chatArea.append("You: " + message + "\n");
+                messageField.setText("");
+            }
+        }
+    }
+
+    private class ResetButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            messageField.setText("");
+        }
+    }
+    
+}
