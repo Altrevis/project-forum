c0648e0b95cf7c6b2e3599377c0bb79f0a99e4ed
Benazeth Léo <leo.benazeth@ynov.com>
2024-04-09 11:32:28 +0200

    affichage principale pour mettre du texte en java

--- forum/src/main/java/com/oop/App.java
+++ forum/src/main/java/com/oop/App.java
@@ -1,17 +1,40 @@
 package com.oop;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.WindowConstants;
+import javax.swing.*;
+import java.awt.*;
 
-public class App 
-{
-    public static void main(String[] args) {
+class App{
+    public static void main(String args[]) {
 
-        JFrame window = new JFrame("Projet forum java");
-        window.add(new JLabel("Hello world!"));
-        window.setSize(900,900);
-        window.setLocation(0, 0);
-        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        JFrame frame = new JFrame("Chat Frame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+
+        JMenuBar mb = new JMenuBar();
+        JMenu m1 = new JMenu("FILE");
+        JMenu m2 = new JMenu("Help");
+        mb.add(m1);
+        mb.add(m2);
+        JMenuItem m11 = new JMenuItem("Open");
+        JMenuItem m22 = new JMenuItem("Save as");
+        m1.add(m11);
+        m1.add(m22);
+
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("Enter Text");
+        JTextField tf = new JTextField(10);
+        JButton send = new JButton("Send");
+        JButton reset = new JButton("Reset");
+        panel.add(label);
+        panel.add(tf);
+        panel.add(send);
+        panel.add(reset);
+
+        JTextArea ta = new JTextArea();
+
+        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        frame.getContentPane().add(BorderLayout.NORTH, mb);
+        frame.getContentPane().add(BorderLayout.CENTER, ta);
+        frame.setVisible(true);
     }
 }
