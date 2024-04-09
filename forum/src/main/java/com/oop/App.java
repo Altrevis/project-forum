c61ed8f7b20f344c81ed48b1419e71039d4bbe80
Benazeth Léo <leo.benazeth@ynov.com>
2024-04-09 11:13:57 +0200

    affichage projet forum

--- forum/src/main/java/com/oop/App.java
+++ forum/src/main/java/com/oop/App.java
@@ -13,16 +13,5 @@ public class App
         window.setSize(900,900);
         window.setLocation(0, 0);
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        window.setVisible(true);
-
-        UserManager userManager = new UserManager();
-
-        // Inscription
-        userManager.register("utilisateur1", "motdepasse1");
-        userManager.register("utilisateur2", "motdepasse2");
-
-        // Connexion
-        userManager.login("utilisateur1", "motdepasse1"); // devrait afficher "Login successful."
-        userManager.login("utilisateur1", "motdepasse2"); // devrait afficher "Invalid username or password."
     }
 }
