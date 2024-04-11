267e30bd82135aac70a718eb0fa164e604f5cffc
Benazeth Léo <leo.benazeth@ynov.com>
2024-04-11 10:49:50 +0200

    ajout des boutons pour le système login/password

--- forum/src/main/java/com/oop/IDandPassword.java
+++ forum/src/main/java/com/oop/IDandPassword.java
@@ -3,6 +3,7 @@ package com.oop;
 import java.util.HashMap;
 
 public class IDandPassword {
+    
     HashMap<String,String> logininfo = new HashMap<String,String>();
     IDandPassword() {
         logininfo.put("Altrevis", "d!abolo81");
