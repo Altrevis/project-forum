4f538b531935cfe50fbd4358015b3f9a01db85cc
Alexandre Queudeville <alexandre.queudeville@gmail.com>
2024-04-12 12:35:36 +0200

    enregistrement du pseudo dans la base de données

--- forum/src/main/java/com/oop/CreateDB.java
+++ forum/src/main/java/com/oop/CreateDB.java
@@ -9,20 +9,44 @@ public class CreateDB {
 
         try (Connection connection = DriverManager.getConnection(url, user, password)) {
             Statement statement = connection.createStatement();
-            
-            
-            String sqlCreateDatabase = "CREATE DATABASE IF NOT EXISTS db_ynov";
+
+            String sqlCreateDatabase = "CREATE DATABASE IF NOT EXISTS db_forum";
             statement.executeUpdate(sqlCreateDatabase);
-            
-            
-            String sqlUseDatabase = "USE db_ynov";
+
+            String sqlUseDatabase = "USE db_forum";
             statement.executeUpdate(sqlUseDatabase);
-            
-            
-            String sqlCreateTable = "CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY, nom VARCHAR(50))";
+
+            String sqlCreateTable = "CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, nom VARCHAR(15))";
             statement.executeUpdate(sqlCreateTable);
-            
+
             System.out.println("Table créée avec succès !");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void saveUserID(String userID) {
+        String url = "jdbc:mysql://localhost:3306/db_forum";
+        String user = "root";
+        String password = "password";
+    
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            Statement statement = connection.createStatement();
+            
+            // Vérifie si userID existe déjà dans la base de données
+            String sqlSelect = "SELECT COUNT(*) AS count FROM users WHERE nom = '" + userID + "'";
+            ResultSet resultSet = statement.executeQuery(sqlSelect);
+            resultSet.next();
+            int count = resultSet.getInt("count");
+            if (count == 0) {
+                // Si userID n'existe pas, effectue l'insertion
+                String sqlInsert = "INSERT INTO users (nom) VALUES ('" + userID + "')";
+                statement.executeUpdate(sqlInsert);
+                System.out.println("UserID enregistré dans la base de données avec succès !");
+            } else {
+                // Si userID existe déjà, affiche un message d'erreur
+                System.out.println("UserID existe déjà dans la base de données.");
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- forum/src/main/java/com/oop/LoginPage.java
+++ forum/src/main/java/com/oop/LoginPage.java
@@ -12,35 +12,37 @@ import javax.swing.JLabel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-public class LoginPage implements ActionListener{
+public class LoginPage implements ActionListener {
     JFrame frame = new JFrame();
     JButton loginButton = new JButton("Login");
     JButton resetButton = new JButton("Reset");
+
     JTextField userIDField = new JTextField();
     JPasswordField userPasswordField = new JPasswordField();
 
-    JLabel userIDLabel = new JLabel("userID");
-    JLabel userPasswordLabel = new JLabel("password:");
+    JLabel userIDLabel = new JLabel("UserID");
+    JLabel userPasswordLabel = new JLabel("Password:");
     JLabel messageLabel = new JLabel("");
 
-    HashMap<String,String> logininfo = new HashMap<String,String>();
-    LoginPage(HashMap<String,String> loginInfoOriginal) {
+    HashMap<String, String> logininfo = new HashMap<String, String>();
+    private String userID;
 
+    LoginPage(HashMap<String, String> loginInfoOriginal) {
         logininfo = loginInfoOriginal;
-        userIDLabel.setBounds(50,100,75,25);
-        userPasswordLabel.setBounds(50,150,75,25);
+        userIDLabel.setBounds(50, 100, 75, 25);
+        userPasswordLabel.setBounds(50, 150, 75, 25);
 
-        messageLabel.setBounds(125,250,250,35);
-        messageLabel.setFont(new Font(null,Font.ITALIC,25));
+        messageLabel.setBounds(125, 250, 250, 35);
+        messageLabel.setFont(new Font(null, Font.ITALIC, 25));
 
-        userIDField.setBounds(125,100,200,25);
-        userPasswordField.setBounds(125,150,200,25);
+        userIDField.setBounds(125, 100, 200, 25);
+        userPasswordField.setBounds(125, 150, 200, 25);
 
-        loginButton.setBounds(125,200,100,25);
+        loginButton.setBounds(125, 200, 100, 25);
         loginButton.setFocusable(false);
         loginButton.addActionListener(this);
 
-        resetButton.setBounds(225,200,100,25);
+        resetButton.setBounds(225, 200, 100, 25);
         resetButton.setFocusable(false);
         resetButton.addActionListener(this);
 
@@ -52,34 +54,34 @@ public class LoginPage implements ActionListener{
         frame.add(loginButton);
         frame.add(resetButton);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(420,420);
+        frame.setSize(420, 420);
         frame.setLayout(null);
         frame.setVisible(true);
-        
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource()==resetButton) {
+        if (e.getSource() == resetButton) {
             userIDField.setText("");
             userPasswordField.setText("");
         }
-        if(e.getSource()==loginButton) {
-            String userID = userIDField.getText();
+        if (e.getSource() == loginButton) {
+            userID = userIDField.getText();
             String password = String.valueOf(userPasswordField.getPassword());
-            if(logininfo.containsKey(userID)) {
-                if(logininfo.get(userID).equals(password)) {
+            if (logininfo.containsKey(userID)) {
+                if (logininfo.get(userID).equals(password)) {
                     messageLabel.setForeground(Color.green);
-                    messageLabel.setText("Login succesful");
+                    messageLabel.setText("Login successful");
+                    CreateDB.saveUserID(userID); // save userID in db_forum
                     new ForumScreen(userID);
-                }
-                else {
+                    frame.dispose();
+                } else {
                     messageLabel.setForeground(Color.red);
                     messageLabel.setText("Wrong password");
                 }
-            }
-            else {
+            } else {
                 messageLabel.setForeground(Color.red);
-                messageLabel.setText("username not found");
+                messageLabel.setText("Username not found");
             }
         }
     }
